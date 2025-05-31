1212b83f12f5d0a418bfc167faa1b60e12108c91
pratikpaudel <prateekpaudel2017@gmail.com>
2025-05-30 22:19:57 -0500

    login view and the registration view work.

---
+++ ToDoList/Views/AuthTextField.swift
@@ -0,0 +1,18 @@
+//
+//  AuthTextField.swift
+//  ToDoList
+//
+//  Created by Guest Account on 5/30/25.
+//
+
+import SwiftUI
+
+struct AuthTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthTextField()
+}
