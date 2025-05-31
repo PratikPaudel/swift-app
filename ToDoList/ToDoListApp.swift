d8e93eb437704eeb45d9d4af3e06067bbed653b8
pratikpaudel <prateekpaudel2017@gmail.com>
2025-05-30 19:44:27 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Guest Account on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
